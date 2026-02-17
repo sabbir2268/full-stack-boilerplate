f08fd9e00f0d2ef4823d4f6eeab46287b5692876
Md Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2026-02-17 11:24:44 +0600

    template structure 1.0

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="text-center mt-20 font-bold">
+        <h1>Welcome to React template</h1>
+        <h1>Tailwind + daisyUI + fonts</h1>
+        <h1>Updated</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>Main layout after change</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
